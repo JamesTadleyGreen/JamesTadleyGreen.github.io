03c792f022311db38fa50ca1b85e2a82f88ac08f
JamesTadleyGreen <JamesTadleyGreen@outlook.com>
2024-03-07 08:49:52 +0000

    Build: Run

--- docs/code/the_last_algorithms_course/1-intro.py
+++ docs/code/the_last_algorithms_course/1-intro.py
@@ -5,12 +5,30 @@ def my_func(x, y):
 
 print(my_func(1, 2))
 # 3
+# §
 
 
 # §sum
-def sum(x, y):
+def um(x, y):
     return x + y
 
 
 print(sum(2, 2))
 # 4
+# §
+
+
+
+
+
+
+
+
+# §frankie
+def frankie(x, y):
+    return x + y
+
+
+print(sum(2, 2))
+# 4
+# §
