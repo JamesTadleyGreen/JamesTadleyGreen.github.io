b0524e41cc391e03e5d98e8f276c669ff3bbd018
James Green <JamesTadleyGreen@outlook.com>
2024-08-27 22:31:22 +0100

    feat: updating code numbered css

--- code/haskell-programming-from-first-principles/8-chapter.hs
+++ code/haskell-programming-from-first-principles/8-chapter.hs
@@ -46,4 +46,6 @@ digits n = n `mod` 10 : digits (n `div` 10)
 wordNumber :: Int -> String
 wordNumber n = concat $ intersperse "-" $ map digitToWord $ reverse $ digits n
 
+hellow this is a realllllllllly long line of text getting longer and longer and reallllllly lomng now
+
 -- §
