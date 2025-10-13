1b7b05f1f7239786ac0312b5e76f9ace98aa45af
Jamie Green <jamiegreen@DESKTOP-JUL1I9U.communityfibre.co.uk>
2025-10-13 23:21:08 +0100

    feat: updating pandoc version

--- Site/Compiler.hs
+++ Site/Compiler.hs
@@ -1,21 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Site.Compiler
-  ( pandocCompiler'
-  ) where
-
-import Site.Snippet (codeInclude, pandocHighlightingStyle)
+  ( pandocCompiler',
+  )
+where
 
 import qualified Data.Map as M
 import Data.Text (Text)
 import Hakyll
+import Site.Snippet (codeInclude, pandocHighlightingStyle)
 import Text.Pandoc.Options
-  ( Extension(..)
-  , HTMLMathMethod(MathJax)
-  , ReaderOptions(..)
-  , WriterOptions(..)
-  , extensionsFromList
-  , readerExtensions
+  ( Extension (..),
+    HTMLMathMethod (MathJax),
+    HighlightMethod (..),
+    ReaderOptions (..),
+    WriterOptions (..),
+    extensionsFromList,
+    readerExtensions,
   )
 
 pandocCompiler' :: M.Map Text (Int, Text) -> Compiler (Item String)
@@ -23,23 +24,23 @@ pandocCompiler' snippets =
   pandocCompilerWithTransform
     defaultHakyllReaderOptions
       { readerExtensions =
-          readerExtensions defaultHakyllReaderOptions <>
-          extensionsFromList
-            [ Ext_tex_math_single_backslash -- TeX math btw (..) [..]
-            , Ext_tex_math_double_backslash -- TeX math btw \(..\) \[..\]
-            , Ext_tex_math_dollars -- TeX math between $..$ or $$..$$
-            , Ext_latex_macros -- Parse LaTeX macro definitions (for math only)
-            , Ext_inline_code_attributes -- Ext_inline_code_attributes
-            , Ext_abbreviations -- PHP markdown extra abbreviation definitions
-            , Ext_fenced_divs
-            ]
+          readerExtensions defaultHakyllReaderOptions
+            <> extensionsFromList
+              [ Ext_tex_math_single_backslash, -- TeX math btw (..) [..]
+                Ext_tex_math_double_backslash, -- TeX math btw \(..\) \[..\]
+                Ext_tex_math_dollars, -- TeX math between $..$ or $$..$$
+                Ext_latex_macros, -- Parse LaTeX macro definitions (for math only)
+                Ext_inline_code_attributes, -- Ext_inline_code_attributes
+                Ext_abbreviations, -- PHP markdown extra abbreviation definitions
+                Ext_fenced_divs
+              ]
       }
     defaultHakyllWriterOptions
-      { writerHighlightStyle = Just pandocHighlightingStyle
-      , writerHTMLMathMethod = MathJax ""
-      , writerTableOfContents = True
-      , writerNumberSections = True
-      , writerTOCDepth = 2
-      -- , writerTemplate = Just tocTemplate
+      { writerHighlightMethod = Skylighting pandocHighlightingStyle,
+        writerHTMLMathMethod = MathJax "",
+        writerTableOfContents = True,
+        writerNumberSections = True,
+        writerTOCDepth = 2
+        -- , writerTemplate = Just tocTemplate
       }
     (codeInclude snippets)
--- Site/Snippet.hs
+++ Site/Snippet.hs
@@ -1,19 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Site.Snippet
-  ( fileToSnippet
-  , codeInclude
-  , pandocHighlightingStyle
-  ) where
+  ( fileToSnippet,
+    codeInclude,
+    pandocHighlightingStyle,
+  )
+where
 
 import Data.List (intercalate)
 import Data.List.Split (splitOn)
 import Data.Map (Map, fromList)
 import qualified Data.Map as M
-import Data.Maybe (maybeToList)
+import Data.Maybe (fromMaybe, maybeToList)
 import Data.Text (Text, pack)
 import Hakyll (Item, itemBody)
-import Text.Pandoc.Definition (Block(CodeBlock, Div, Null), Pandoc)
+import Text.Pandoc.Definition (Block (CodeBlock, Div), Pandoc)
 import Text.Pandoc.Highlighting (Style, breezeDark)
 import Text.Pandoc.Walk (walk)
 
@@ -23,14 +24,17 @@ fileToSnippet item = fromList (map snd kvs)
     snippets = splitOn "§" (itemBody item)
     kvs =
       scanl
-        (\(lineNumber, (_, (_, _))) ->
-           extractNameAndLineNumber lineNumber . lines)
+        ( \(lineNumber, (_, (_, _))) ->
+            extractNameAndLineNumber lineNumber . lines
+        )
         (0, ("", (0, "")))
         snippets
     extractNameAndLineNumber lineNumber s =
-      ( lineNumber + length s - 1
-      , ( pack $ head s
-        , (lineNumber + 2, pack $ intercalate "\n" $ tail $ init s)))
+      ( lineNumber + length s - 1,
+        ( pack $ head s,
+          (lineNumber + 2, pack $ intercalate "\n" $ tail $ init s)
+        )
+      )
 
 codeInclude :: Map Text (Int, Text) -> Pandoc -> Pandoc
 codeInclude snippets =
@@ -38,21 +42,22 @@ codeInclude snippets =
     case block of
       div@(Div (_, cs, _) _) ->
         if "code-include" `elem` cs
-          then codeBlockFromDiv snippets div
+          then fromMaybe block $ codeBlockFromDiv snippets div
           else block
       _ -> block
 
-codeBlockFromDiv :: Map Text (Int, Text) -> Block -> Block
+codeBlockFromDiv :: Map Text (Int, Text) -> Block -> Maybe Block
 codeBlockFromDiv snippets div@(Div (_, _, kvs) _) =
   let snippet = lookup "name" kvs >>= (`M.lookup` snippets)
       classes = maybeToList (lookup "lexer" kvs)
    in case snippet of
-        Nothing -> Null
+        Nothing -> Nothing
         Just (lineNumber, content) ->
-          CodeBlock
-            ("", classes, [("startFrom", pack $ show lineNumber)])
-            content
-codeBlockFromDiv _ _ = Null
+          Just $
+            CodeBlock
+              ("", classes, [("startFrom", pack $ show lineNumber)])
+              content
+codeBlockFromDiv _ _ = Nothing
 
 --------------------------------------------------------------------------------
 -- tocTemplate = do
